90a419205a4e6112110f6481105afddfca08055a
Christian Albert <chrs.albert@gmail.com>
2026-02-01 15:12:37 +0100

    Force page rendering on locale change

--- app/app.vue
+++ app/app.vue
@@ -1,14 +1,17 @@
 <template>
   <NuxtLayout>
-    <NuxtPage />
+    <NuxtPage :key="pageKey" />
   </NuxtLayout>
 </template>
 
 <script setup lang="ts">
+const route = useRoute();
 const { locale, fallbackLocale } = useI18n();
 const currentLocale = computed(
   () => (locale.value || fallbackLocale.value) as string,
 );
+
+const pageKey = computed(() => `${locale.value}:${route.fullPath}`);
 
 useHead(() => ({
   htmlAttrs: { lang: currentLocale.value },
