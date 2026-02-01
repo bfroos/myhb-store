a3c71c7fe4f23ce7e6eec4abeb0725d6f475e4cc
Christian Albert <chrs.albert@gmail.com>
2026-02-01 15:45:29 +0100

    Update [...path].get.ts

--- server/api/strapi/[...path].get.ts
+++ server/api/strapi/[...path].get.ts
@@ -2,7 +2,7 @@ export default defineEventHandler(async (event) => {
   const config = useRuntimeConfig();
   const incoming = getRequestURL(event);
 
-  setHeader(event, "Cache-Control", "max-age=0, s-maxage=300, stale-while-revalidate=60");
+  setHeader(event, "Cache-Control", "no-store");
 
   if (!config.public.strapiUrl) {
     throw createError({ statusCode: 500, statusMessage: "Strapi URL missing" });
