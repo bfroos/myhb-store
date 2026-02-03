50adea5b4e76ce7e477848021cc63ef685ee2561
Christian Albert <chrs.albert@gmail.com>
2026-02-03 01:37:21 +0100

    Remove viewport maximum scale

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -27,7 +27,7 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
-      viewport: "width=device-width, initial-scale=1, maximum-scale=1",
+      viewport: "width=device-width, initial-scale=1",
       link: [
         {
           rel: "icon",
