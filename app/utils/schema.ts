e8cd65a2a6b7da755d0774564366edc8e2d5a3c6
Christian Albert <chrs.albert@gmail.com>
2026-02-04 19:17:01 +0100

    Fix duplicated schema export

--- app/utils/schema.ts
+++ app/utils/schema.ts
@@ -1,7 +1,3 @@
 export { buildBlogPostingSchema } from "~/utils/schemaBlog";
 export { buildJobPostingSchema } from "~/utils/schemaJob";
 export { buildProductSchema } from "~/utils/schemaProduct";
-export type {
-  SchemaOrgContext,
-  WebSiteSchemaContext,
-} from "~/utils/schemaShared";
