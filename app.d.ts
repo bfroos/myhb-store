baed87a9b29d91a7f18e138f47e058d9707beaba
Christian Albert <chrs.albert@gmail.com>
2026-02-13 00:09:19 +0100

    Fix type

--- app.d.ts
+++ app.d.ts
@@ -1,5 +1,13 @@
 declare module "@mailchimp/mailchimp_marketing";
 
+declare module "@nuxt/schema" {
+  interface NuxtConfig {
+    scripts?: {
+      registry?: Record<string, unknown>;
+    };
+  }
+}
+
 declare global {
   interface Window {
     Cookiebot?: {
