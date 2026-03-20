39656cdd938ab03835cf366e452d16c0f987a3df
Christian Albert <chrs.albert@gmail.com>
2026-03-20 15:45:49 +0100

    Disable redirects in ads mode

--- server/middleware/redirects.ts
+++ server/middleware/redirects.ts
@@ -4,6 +4,9 @@ export default defineEventHandler(async (event) => {
   const method = event.method || "GET";
   if (method !== "GET" && method !== "HEAD") return;
 
+  const config = useRuntimeConfig();
+  if (config.public.siteMode === "ads") return;
+
   const { pathname, search } = getRequestURL(event);
   const result = await resolveRedirect(pathname, search);
   if (!result) return;
