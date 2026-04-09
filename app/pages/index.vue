6c0612212caed1143340cb91fecaf78b4018ab17
Christian Albert <chrs.albert@gmail.com>
2026-04-09 13:52:40 +0200

    Add location pages to homepage

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,11 +1,20 @@
 <template>
   <BlockRenderer v-if="blocks && blocks.length > 0" :blocks="blocks" />
+  <BlockLocationTeasers
+    v-if="homepageLocations.length > 0"
+    :headline="$t('locations.discoverLocations')"
+    :locations="homepageLocations"
+    :show-filters="true"
+  />
 </template>
 
 <script setup lang="ts">
 const { fetchHomepage, seo, blocks } = useHomepage();
+const { fetchWithLocations, locations } = useLocationsPage();
+const homepageLocations = computed(() => locations.value.open ?? []);
 
 const homepageLoaded = await fetchHomepage();
+await fetchWithLocations();
 
 if (homepageLoaded) {
   await setPageSeo(seo.value);
