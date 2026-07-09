871acb33529dc50b6a2023431bf68ff098a78ad9
big fucking roocket <b.roos@myhb.app>
2026-07-09 22:24:29 +0200

    feat(booking): add AppBookingDialog iframe component

---
+++ app/components/ui/organism/AppBookingDialog.vue
@@ -0,0 +1,27 @@
+<template>
+  <iframe
+    v-if="params.url"
+    :src="params.url"
+    class="appBookingDialog"
+    title="Termin buchen"
+    allow="clipboard-write; payment"
+    loading="eager"
+  />
+</template>
+<script setup lang="ts">
+const dialogRef = inject("dialogRef") as any;
+const params = ref<any>({});
+
+onMounted(() => {
+  params.value = dialogRef.value.data;
+});
+</script>
+<style scoped>
+.appBookingDialog {
+  width: 100% !important;
+  height: 100% !important;
+  border: 0;
+  display: block;
+  overflow: hidden;
+}
+</style>
