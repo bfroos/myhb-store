39878a2122dbbfdfcda924087fdf3dabe81b86e9
big fucking roocket <b.roos@myhb.app>
2026-07-17 15:48:18 +0100

    SEO Block 2: Offer/ItemList-Schema auf /preise einbinden

--- app/pages/preise.vue
+++ app/pages/preise.vue
@@ -13,4 +13,14 @@ const pageLoaded = await fetchPage();
 if (pageLoaded) {
   await setPageSeo(seo.value);
 }
+
+// Schema.org ItemList (Service + Offer) for the treatment prices
+const config = useRuntimeConfig();
+const priceListSchema = computed(() =>
+  buildPriceListSchema(
+    productCategories.value,
+    (config.public.publicUrl as string) || "",
+  ),
+);
+useSchemaOrg(priceListSchema);
 </script>
